f75258757ebe3c23050a66169a3f1777ca9cd0ef
Deepak Play <deepakplay14@gmail.com>
2021-02-09 18:56:25 +0530

    UI

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 	</header>
 
 	<form method="post" action="/login">
+		<h2>Login</h2>
 		<div class="email">
 			<label for="email">Email :</label>
 			<input type="text" name="email" id="email" autocomplete="off">	
@@ -28,6 +29,28 @@
 			<input type="password" name="pass" id="pass">	
 		</div>
 		<input type="submit" value="Login" class="submit">
+		<span class="new_site">New to this site? <a href="/register">Register</a></span>
+	</form>
+
+
+	<form method="post" action="/login" >
+		<h2>Register</h2>
+
+		<div class="name">
+			<label for="name">Full name :</label>
+			<input type="text" name="name" id="name" autocomplete="off">	
+		</div>
+		<div class="email">
+			<label for="email">Email :</label>
+			<input type="text" name="email" id="email" autocomplete="off">	
+		</div>
+		
+		<div class="pass">
+			<label for="pass">Password :</label>
+			<input type="password" name="pass" id="pass">	
+		</div>
+		<input type="submit" value="Login" class="submit">
+		<span class="new_site">New to this site? <a href="/register">Register</a></span>
 	</form>
 	<footer>
 		<p>Copyright &copy; <?php echo date('Y')?> <a href="/deepak">Deepak Kumar</a></p>
