91466ef74cc61ba951b192118e2c3f5b89e55ecc
Deepak Play <deepakplay14@gmail.com>
2021-02-10 21:56:55 +0530

    Updated...

--- users.sql
+++ users.sql
@@ -1,22 +1,21 @@
 create table users(
 	id int auto_increment primary key,
 	fname varchar(50) not null,
-	email varchar(50) not null,
-	pass varchar(60) not null
+    email varchar(50) not null,
+    pass varchar(60) not null
 );
 
 
 create table user_table(
-	user_id int not null primary key,
- 	gender enum('m', 'f'),
- 	phone varchar(12),
- 	dob date default '2000/01/01',
- 	country varchar(25) default 'India',
- 	state varchar(25) default 'Tamilnadu',
- 	foreign key (user_id) references users(id)
- 		on delete cascade
- 		on update cascade
- 	);
+	user_id int primary key,
+    gender enum('m', 'f'),
+    phone varchar(12),
+    dob date default '2000/01/01',
+    country varchar(25) default 'India',
+    state varchar(25) default 'Tamilnadu',
+    foreign key (user_id) references users(id)
+		on delete cascade on update cascade
+);
 
 
 insert into user_table (user_id, gender, phone, dob, country, state) values (1, 'm', '+91987654321', '1998-07-14', 'India', 'Tamilnadu');
