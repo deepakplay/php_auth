1c31b5ad3055d8d70e535a640814fa28b8e89727
Deepak Play <deepakplay14@gmail.com>
2021-02-10 11:57:24 +0530

    Finished UI

--- index.php
+++ index.php
@@ -5,55 +5,15 @@
 	<meta charset="utf-8" >
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<script src="assets/script.js" defer></script>
 </head>
 <body>
-	<header>
-		<span class="logo">PHP Site</span>
-		<nav>			
-			<ul>
-				<li><a href="/login">Login</a></li>
-				<li><a href="/register">Register</a></li>
-			</ul>
-		</nav>
-	</header>
+	<?php include('includes/header.php')?>
 
-	<form method="post" action="/login">
-		<h2>Login</h2>
-		<div class="email">
-			<label for="email">Email :</label>
-			<input type="text" name="email" id="email" autocomplete="off">	
-		</div>
-		
-		<div class="pass">
-			<label for="pass">Password :</label>
-			<input type="password" name="pass" id="pass">	
-		</div>
-		<input type="submit" value="Login" class="submit">
-		<span class="new_site">New to this site? <a href="/register">Register</a></span>
-	</form>
+	<?php include('includes/login_form.php') ?>
 
+	<?php include('includes/register_form.php') ?>
 
-	<form method="post" action="/login" >
-		<h2>Register</h2>
-
-		<div class="name">
-			<label for="name">Full name :</label>
-			<input type="text" name="name" id="name" autocomplete="off">	
-		</div>
-		<div class="email">
-			<label for="email">Email :</label>
-			<input type="text" name="email" id="email" autocomplete="off">	
-		</div>
-		
-		<div class="pass">
-			<label for="pass">Password :</label>
-			<input type="password" name="pass" id="pass">	
-		</div>
-		<input type="submit" value="Login" class="submit">
-		<span class="new_site">New to this site? <a href="/register">Register</a></span>
-	</form>
-	<footer>
-		<p>Copyright &copy; <?php echo date('Y')?> <a href="/deepak">Deepak Kumar</a></p>
-	</footer>
+	<?php include('includes/footer.php')?>
 </body>
 </html>
